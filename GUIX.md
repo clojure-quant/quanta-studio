8eb7d86ed5665ef9556736ae9d7554d312a4b3b0
awb99 <awb99@gmail.com>
2024-09-22 15:12:34 -0500

    aelph curl test added

---
+++ GUIX.md
@@ -0,0 +1,14 @@
+
+
+java --version
+
+
+
+dev environment with specific version of java openjdk:
+
+guix shell --development --check openjdk@21:jdk
+
+
+now in the shell run
+java --version 
+and you see the different version
---
+++ src-junk/aleph/src/demo.clj
@@ -0,0 +1,12 @@
+(ns demo
+  (:require
+   [aleph.http :as http]))
+
+
+
+(defn download-link [& _]
+  (let [url "https://www.google.com"
+        _ (println  "downloading link: " url)      
+        request @(http/get url {:socket-timeout 90000
+                                :connection-timeout 90000})]
+    (println request)))
