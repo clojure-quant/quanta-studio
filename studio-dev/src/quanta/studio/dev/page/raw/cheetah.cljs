760bfbdfb55dbf41639e2f86e2d1dccc15802e64
awb99 <awb99@gmail.com>
2024-10-12 14:40:06 -0500

    cheetah-roundtrip

--- studio-dev/src/quanta/studio/dev/page/raw/cheetah.cljs
+++ studio-dev/src/quanta/studio/dev/page/raw/cheetah.cljs
@@ -6,24 +6,32 @@
   [cheetah-ds {:style {:width "100%" :height "100%"}
                :columns [; bar
                          {:field "asset" :caption "a" :width 90}
-                         {:field "entry-date" :caption "d" :width 220}
-                         {:field "entry-price" :caption "o" :width 90
-                          ;:style 'demo.page.cheetah/red-color
-                          }
-                         ;{:field "close" :caption "c" :width 90 :style blue-color}
-                         {:field  "entry-idx" :caption "B?" :width 50 :style {:bgColor "#5f5"}}
-                         {:field  "cross-down" :caption "XD" :width 50}
-                         {:field  "cross-down-c" :caption "XD_" :width 50}
-                         {:field  "long-signal" :caption "LS" :width 50}
-
-                         {:field  "above-band" :caption "A?" :width 50}
-                         {:field  "cross-up" :caption "XU" :width 50}
-                         ;{:field  "cross-up-c" :caption "XU_" :width 50 :style demo.page.cheetah/bool-color}
-                         {:field  "short-signal" :caption "SS" :width 50}
-                         {:field "entry" :caption "entry" :width 50}]
+                         {:field "id" :caption "id" :width 50}
+                         {:field "side" :caption "side" :width 50}
+                         {:field "qty" :caption "qty" :width 50}
+                         ; entry
+                         {:field "entry-date" :caption "entry-dt" :width 200}
+                         {:field "entry-idx" :caption "entry-idx" :width 50 :style {:bgColor "#5f5"}}
+                         {:field "entry-price" :caption "entry-p" :width 90 #_:style #_'demo.page.cheetah/red-color}
+                         ; exit
+                         {:field "exit-date" :caption "exit-dt" :width 200}
+                         {:field "exit-idx" :caption "exit-idx" :width 50 :style {:bgColor "#5f5"}}
+                         {:field "exit-price" :caption "exit-p" :width 50}
+                         {:field "exit-rule" :caption "exit-r" :width 90}
+                         ; metrics
+                         {:field "bars" :caption "bars" :width 50}
+                         {:field "win?" :caption "win?" :width 50}
+                         {:field "ret-abs" :caption "ret-abs" :width 50}
+                         {:field "ret-prct" :caption "ret-prct" :width 50}
+                         {:field "ret-log" :caption "ret-log" :width 50}
+                         ;; nav
+                         {:field "cum-ret-volume" :caption "cum-ret-volume" :width 50}
+                         {:field "cum-ret-abs" :caption "cum-ret-abs" :width 50}
+                         {:field "cum-ret-log" :caption "cum-ret-log" :width 50}
+                         {:field "nav" :caption "nav" :width 50}
+                         ]
                :url "/r/data/LWhgL6-roundtrips.transit-json"}])
 
 (defn page [_route]
   [:div.h-screen.w-screen.bg-red-500
    [cheetah-table]])
-
