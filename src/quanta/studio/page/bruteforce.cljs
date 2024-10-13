ebf2934726e9bfc6c75cbe167d740f6e21b2c525
awb99 <awb99@gmail.com>
2024-10-13 08:42:47 -0500

    bruteforcer-ui backtest-result loading ok

--- src/quanta/studio/page/bruteforce.cljs
+++ src/quanta/studio/page/bruteforce.cljs
@@ -9,8 +9,7 @@
    [goldly.service.core :refer [clj]]
    [ajax.promise :refer [GET]]
    [clojure.edn :refer [read-string]]
-   [quanta.studio.view.bruteforce :refer [bruteforce-result-ui]]
-   ))
+   [quanta.studio.view.bruteforce :refer [bruteforce-result-ui bruteforce-roundtrips]]))
 
 (defn load-label [label result-a]
   (let [url (str "/r/bruteforce/" label ".edn")
@@ -18,16 +17,12 @@
         get-p (GET url)]
     (-> get-p
         (p/then (fn [txt]
-                  (println "load-label success: " txt)
-                  (try 
+                  (println "load-label success ") ;  txt
+                  (try
                     (reset! result-a (read-string txt))
                     ;(reset! result-a txt)
                     (catch js/Exception ex
-                      (println "parse-exception: " ex)
-                      )
-                  )
-                  
-                  ))
+                      (println "parse-exception: " ex)))))
         (p/catch (fn [err]
                    (println "error: could not load result for label: " label "url: " url " err: " err)
                    (reset! result-a nil))))))
@@ -36,7 +31,7 @@
   (let [rp (clj 'quanta.studio.bruteforce/show-available)]
     (-> rp
         (p/then (fn [labels]
-                  (println "get-labels success: " labels)
+                  (println "get-labels success ")  ; labels
                   (reset! labels-a (into [] labels))))
         (p/catch (fn [err]
                    (println "get-labels error: " err)
@@ -76,7 +71,11 @@
 (defn bruteforce-ui []
   (let [state {:labels-a (r/atom [])
                :options-a (r/atom {:label nil})
-               :result-a (r/atom nil)}]
+               :result-a (r/atom nil)
+               :id-a (r/atom nil)}
+        set-id (fn [id] 
+                 (when-not (= id @(:id-a state))
+                   (reset! (:id-a state) id)))]
     (get-labels (:labels-a state))
     (add-watch (:options-a state) :watcher
                (fn [key atom old-state new-state]
@@ -89,21 +88,27 @@
                    (println "requesting result for : " label)
                    (load-label label (:result-a state)))))
     (fn []
-      [:div.flex.flex-col.h-full.w-full
-       [header state]
-       ;[rtable.rtable/rtable table-opts cols @(:tasks-a state)]
-       [:div.bg-red-500.p-5.m-5
-        [:p (pr-str @(:labels-a state))
-         (pr-str @(:options-a state))
-         (pr-str @(:result-a state))
-         (pr-str (:template-id @(:result-a state)))]]
-       [bruteforce-result-ui @(:result-a state)]
-       ])))
+      [spaces.core/viewport
+       [spaces.core/top-resizeable {:size 50}
+        [header state]]
+       [spaces.core/fill
+        [spaces.core/left-resizeable {:size 500}
+         [:div.bg-red-500.p-5.m-5
+          #_[:p (pr-str @(:labels-a state))
+           (pr-str @(:options-a state))
+           (pr-str @(:result-a state))
+           (pr-str (:template-id @(:result-a state)))]
+           [bruteforce-result-ui @(:result-a state) set-id]]]
+        [spaces.core/fill 
+        [:div.bg-blue-300
+         [bruteforce-roundtrips (-> state :options-a deref :label) 
+                                (-> state :id-a deref)]
+         
+          
+         ]]]])))
 
 (defn page [_route]
-  [:div.h-screen.w-screen.bg-red-500
-   [bruteforce-ui]])
-
+  [bruteforce-ui])
 
 (def data [:a 1 :b "xxx"])
 
--- src/quanta/studio/view/bruteforce.cljs
+++ src/quanta/studio/view/bruteforce.cljs
@@ -1,19 +1,88 @@
-(ns quanta.studio.view.bruteforce)
+(ns quanta.studio.view.bruteforce
+  (:require
+   [reagent.core :as r]
+   [promesa.core :as p]
+   [rtable.rtable]
+   [cquant.tmlds :refer [GET]]
+   [quanta.viz.render.trade.core :refer [roundtrip-stats-ui]]))
 
-(defn bruteforce-result-ui [{:keys [label template-id calculated algo variations result]}]
-  [:div.grid.grid-cols-2
-   [:p "label"] [:p label]
-   [:p "template-id"] [:p template-id]
-   [:p "calculated"] [:p calculated]
-   [:p "variations"] [:p (pr-str variations)]
-   [:p "result"] [:p (count result)]])
+;; bruteforce roundtrip-ui
 
+(def stats-opts
+  {:intraday? false
+   :style {:height "600px"
+           :width "800px"}
+   :class "bg-red-500"})
 
-;;:variations [[0 :asset] ["BTCUSDT" "ETHUSDT"] [2 :day :atr-n] [20 50]],
-{[0 :asset] "BTCUSDT",
- [2 :day :atr-n] 50,
- :target 0.5148529360496968,
- :trades 178,
- :cum-pl -31625.764798952816,
- :max-dd 32251.10599895282,
- :id "YRajWa"} "
+(defn load-backtest [label id backtest-a]
+  (let [url (str "/r/bruteforce/" label "/" id "-backtest.transit-json")
+        _   (println "loading backtest from url: " url)
+        load-promise (GET url)]
+    (-> load-promise
+        (p/then (fn [ds]
+                  (println "ds from url " url " loaded successfully.")
+                  (reset! backtest-a ds)
+                  ds))
+        (p/catch (fn [err]
+                   (println "could not load ds from url " url " err: " err)
+                   (reset! backtest-a nil))))
+    nil))
+
+(defn bruteforce-roundtrips [label id]
+  (let [loaded-a (r/atom [nil nil])
+        backtest-a (r/atom nil)]
+    (fn [label id]
+        (when (and label id
+                   (not (= [label id] @loaded-a)))
+          (println "loading backtest label: " label " id:  " id)
+          (load-backtest label id backtest-a)
+          (reset! loaded-a [label id])  
+          nil)
+        (if @backtest-a
+          [roundtrip-stats-ui stats-opts @backtest-a]
+          [:div "label:" label  " id: " id]))))
+
+;; bruteforce label results
+
+(def table-opts
+  {:class "table-head-fixed padding-sm table-blue table-striped table-hover"
+   :style {:width "100%"
+           :height "100%"
+           :border "3px solid green"}})
+
+(defn make-render-id  [set-id]
+  (fn [col-info row]
+    (let [id (:id row)]
+      [:div
+       [:a {:on-click #(set-id id)}
+  ; font awesome v5
+        [:i {:class "fas fa-eye m-1"}]]
+       [:a {;:on-click 
+            }
+        [:i {:class "far fa-stop-circle m-1"}]]
+       [:span.text-blue-500 id]])))
+
+(defn make-cols  [variations set-id]
+  ; :variations [[0 :asset] ["BTCUSDT" "ETHUSDT"] 
+  ;              [2 :day :atr-n] [20 50]]
+  [{:path :id :header "id" :render-cell (make-render-id set-id)}
+   ; [0 :asset] "BTCUSDT",
+   ; [2 :day :atr-n] 50,
+   {:path :target}
+   {:path :trades}
+   {:path :cum-pl}
+   {:path :max-dd}])
+
+(defn bruteforce-result-ui [{:keys [label template-id calculated algo variations result]}
+                            set-id]
+  [:div.flex.flex-col
+   [:div.grid.grid-cols-2
+    [:p "label"] [:p label]
+    [:p "template-id"] [:p template-id]
+    [:p "calculated"] [:p calculated]
+    [:p "variations"] [:p (pr-str variations)]
+    [:p "result"] [:p (count result)]]
+
+   [rtable.rtable/rtable table-opts (make-cols variations set-id) result]])
+
+;
