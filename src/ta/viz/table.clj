c90413105636331ac277a8847b5d53c1acc57dd8
awb99 <awb99@gmail.com>
2026-04-14 20:00:08 -0500

    move-out to dali-table

--- src/ta/viz/table.clj
+++ src/ta/viz/table.clj
@@ -13,15 +13,4 @@
        {:box :lg
         :data (ds->table ds)}])
 
-(defn flex-item [name ui]
-  [:div  {:style {:background-color "orange"}}
-   [:b {:style {:color "blue"}} name]]
-  [:div
-   ui])
-
-(defn flexbox [data]
-  ^:R [:div {:style {:width 500 :max-width 500 :display "inline-block"}}
-       [:div {:style {:display "flex" :flex-direction "column" :width 500}}
-
-        data]])
 
