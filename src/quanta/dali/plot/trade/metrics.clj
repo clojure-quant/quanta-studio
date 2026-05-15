f167a55bac787ef5e09aeba45912a617a069674f
awb99 <awb99@gmail.com>
2026-05-15 15:58:37 -0500

    dali trade move out

--- src/quanta/dali/plot/trade/metrics.clj
+++
@@ -1,8 +0,0 @@
-(ns quanta.dali.plot.trade.metrics
-  (:require
-   [dali.spec :refer [create-dali-spec]]))
-
-(defn metrics-ui [metrics]
-  (create-dali-spec
-   {:viewer-fn 'quanta.dali.viewer.trade.metrics/metrics-view
-    :data metrics}))
